c0db42ad60dd09db3d520ffe5e6b520d292844f8
Biruk Adera <aderabiruk@gmail.com>
2020-04-19 21:53:29 +0300

    Constants Prepared

---
+++ src/utils/constants.js
@@ -0,0 +1,29 @@
+export const BOOLEAN_VALIDATOR_TYPES = {
+    EQUAL: "equal",
+    REQUIRED: "required"
+};
+
+export const DATE_VALIDATOR_TYPES = {
+    AFTER: "after",
+    BEFORE: "before",
+    EQUAL: "equal",
+    REQUIRED: "required"
+};
+
+export const NUMBER_VALIDATOR_TYPES = {
+    EQUAL: "equal",
+    IN: "in",
+    INTEGER: "integer",
+    MAX: "max",
+    MIN: "min",
+    REQUIRED: "required",
+};
+
+export const STRING_VALIDATOR_TYPES = {
+    EMAIL: "email",
+    EQUAL: "equal",
+    IN: "in",
+    MAXLENGTH: "maxlength",
+    MINLENGTH: "minlength",
+    REQUIRED: "required",
+};
